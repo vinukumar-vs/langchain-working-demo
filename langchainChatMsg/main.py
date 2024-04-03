409311c05242d50a5093beb83f69c8d9b7bae3fb
vinukumar-vs <vinu.kumar@tarento.com>
2024-04-03 23:08:29 +0530

    Output parser to convert the chat message to a string

--- langchainChatMsg/main.py
+++ langchainChatMsg/main.py
@@ -2,6 +2,9 @@ import openai;
 from envVariables import getEnv;
 from langchain_openai import ChatOpenAI;
 from langchain_core.prompts import ChatPromptTemplate;
+from langchain_core.output_parsers import StrOutputParser
+
+output_parser = StrOutputParser()
 
 prompt = ChatPromptTemplate.from_messages([
     ("system", "You are world class technical documentation writer."),
@@ -15,6 +18,6 @@ llm = ChatOpenAI(openai_api_key=openaiKey)
 
 
 def getData(input):
-    chain = prompt | llm
+    chain = prompt | llm | output_parser
     response = chain.invoke(input)
     print(response)
